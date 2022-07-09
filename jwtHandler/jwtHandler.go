f82b41047eaa44a17b4a80177e2d48a54364bfea
Dongkey-Dev <dongkeydev@gmail.com>
2022-07-10 00:58:11 +0900

    feat : Add GetJWTClaims

--- jwtHandler/jwtHandler.go
+++ jwtHandler/jwtHandler.go
@@ -1,19 +1,26 @@
 package jwtHandler
 
-import(
-  "time"
-  "t_unicorn/meth"
-  "github.com/golang-jwt/jwt/v4"
+import (
+	"t_unicorn/meth"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
-func CreateJWT(Email string) (string, error){
-  signingKey := []byte("t_unicorn") //need to change local env file
-  Tok := jwt.New(jwt.SigningMethodHS256)
-  claims := Tok.Claims.(jwt.MapClaims)
-  claims["Email"] = Email
-  claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+func GetJWTClaims() jwt.Claims {
+	Tok := jwt.New(jwt.SigningMethodHS256)
+	claims := Tok.Claims.(jwt.MapClaims)
+	return claims
+}
 
-  tk, err := Tok.SignedString(signingKey)
-  meth.CheckErr(err)
-  return tk, nil
+func CreateJWT(Email string) (string, error) {
+	signingKey := []byte("t_unicorn") //need to change local env file
+	Tok := jwt.New(jwt.SigningMethodHS256)
+	claims := Tok.Claims.(jwt.MapClaims)
+	claims["Email"] = Email
+	claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+
+	tk, err := Tok.SignedString(signingKey)
+	meth.CheckErr(err)
+	return tk, nil
 }
