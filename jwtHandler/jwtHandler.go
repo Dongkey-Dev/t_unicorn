0970089b6856d414235ba9356d5332e25051a8fd
Dongkey-Dev <dongkeydev@gmail.com>
2022-07-09 21:47:05 +0900

    feat : Add jwt creator

---
+++ jwtHandler/jwtHandler.go
@@ -0,0 +1,20 @@
+package jwtHandler
+
+import(
+  "os"
+  "time"
+  "t_unicorn/meth"
+  "github.com/golang-jwt/jwt/v4"
+)
+
+func CreateJWT(Email string) (string, error){
+  signingKey := []byte("t_unicorn") //need to change local env file
+  Tok := jwt.New(jwt.SigningMethodHS256)
+  claims := Tok.Claims.(jwt.MapClaims)
+  claims["Email"] = Email
+  claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+
+  tk, err := Tok.SignedString(signingKey)
+  meth.CheckErr(err)
+  return tk, nil
+}
