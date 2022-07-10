0eada9b81240e72caa9bcae1301977adaade527d
Dongkey-Dev <dongkeydev@gmail.com>
2022-07-11 02:53:41 +0900

    refactor : GetJWTSignature

--- dbHandler/dbHandler.go
+++ dbHandler/dbHandler.go
@@ -15,7 +15,9 @@ func SetupDB() *sql.DB {
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_NAME := os.Getenv("DB_NAME")
-	dbinfo := fmt.Sprintf("user = %s password = %s dbname = %s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	dbinfo := fmt.Sprintf(
+		"user = %s password = %s dbname = %s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
 	db, err := sql.Open("postgres", dbinfo)
 	meth.CheckErr(err)
 	return db
